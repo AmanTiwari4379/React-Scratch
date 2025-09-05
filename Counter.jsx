3bfef3a774266be9ca72bc7800fa6dce64403afa
AmanTiwari4379 <taman4379@gmail.com>
2025-09-05 11:54:14 +0530

    Add each lecture's content

---
+++ Counter.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    const [rcount, setRcount] = useState(10);
+  return (
+    <div>
+      <h3>Count: {count}</h3>
+      <h3>Reverse Count: {rcount}</h3>
+      <button onClick={()=> setCount(count + 1)}>Count</button>
+      <button onClick={()=> setRcount(rcount - 1)}>Reverse Count</button> 
+    </div>
+  )
+}
+
+export default Counter
---
+++ ReactIntro.jsx
@@ -0,0 +1,10 @@
+function ReactIntro(){
+    return(
+        <div>
+            <h4>REACT INTRODUCTION</h4>
+            <img src="https://img.icons8.com/?size=100&id=122637&format=png&color=000000" alt="react logo" />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, tempora et velit quas nihil eaque! Maiores quaerat porro quo quasi nisi repellendus nam totam, quibusdam iure delectus libero rerum magni illum fugit deserunt facere accusamus nihil consequuntur exercitationem eligendi est! Incidunt ratione ad enim. Consequuntur harum necessitatibus perspiciatis nobis tempora!</p>
+        </div>
+    )
+}
+export default ReactIntro;
