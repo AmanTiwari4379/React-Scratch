b6040f55d131f2575853965b5e0728af07a648a5
AmanTiwari4379 <taman4379@gmail.com>
2025-09-06 11:47:27 +0530

    Added userComponent

---
+++ UserComponent.jsx
@@ -0,0 +1,18 @@
+function Login(){
+    return(
+        <div>
+            <h1>Login User</h1>
+        </div>
+    )
+}
+
+export function Profile(){
+    return(
+        <div>
+            <h1>Profile</h1>
+        </div>
+    )
+}
+
+export const UserId = 'A1';
+export default Login; 
