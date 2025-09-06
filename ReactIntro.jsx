128c3124ded1514425e0ce16f8a9df34146843f8
AmanTiwari4379 <taman4379@gmail.com>
2025-09-06 10:33:35 +0530

    Modified heading in abbrev

--- ReactIntro.jsx
+++ ReactIntro.jsx
@@ -1,7 +1,7 @@
 function ReactIntro(){
     return(
         <div>
-            <h4>REACT INTRODUCTION</h4>
+            <h4>REACT INTRO</h4>
             <img src="https://img.icons8.com/?size=100&id=122637&format=png&color=000000" alt="react logo" />
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, tempora et velit quas nihil eaque! Maiores quaerat porro quo quasi nisi repellendus nam totam, quibusdam iure delectus libero rerum magni illum fugit deserunt facere accusamus nihil consequuntur exercitationem eligendi est! Incidunt ratione ad enim. Consequuntur harum necessitatibus perspiciatis nobis tempora!</p>
         </div>
