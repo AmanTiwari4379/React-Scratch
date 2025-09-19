16bd254d61e0bdbbe1e2ebc6c278e9bff9b2354c
AmanTiwari4379 <taman4379@gmail.com>
2025-09-19 16:25:46 +0530

    The user's detail print using map function

---
+++ pasteInApp.jsx/LoopMap.jsx
@@ -0,0 +1,45 @@
+function LoopMap(){
+    const userData = [
+    {
+        name: "Aman",
+        email: "aman@gmail.com",
+        id: 1
+    },
+    {
+        name: "Arjun",
+        email: "arjun@gmail.com",
+        id: 2
+    },
+    {
+        name: "Abhimanyu",
+        email: "abhimanyu@gmail.com",
+        id: 3
+    }
+]
+    return(
+        <div>
+            <h2>Loop in jsx with Map Function</h2>
+            <table border="1">
+                <thead>
+                <tr>
+                    <td>Id</td>
+                    <td>Name</td>
+                    <td>Email</td>
+                </tr>
+                </thead>
+                <tbody>
+                    {
+                        userData.map((user)=>(
+                    <tr>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                    </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+export default LoopMap;
