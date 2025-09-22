9e981f295d864cd38b992a6d0461e202c005a7c8
AmanTiwari4379 <taman4379@gmail.com>
2025-09-22 16:51:41 +0530

    Added Resuse component demo file

---
+++ User.jsx
@@ -0,0 +1,16 @@
+const User = ({data})=>{
+    return(
+        <div style={{
+            border: "1px solid",
+            padding: "10px",
+            margin: "10px",
+            width: "400px",
+            borderRadius: "10px"
+        }}>
+            <h3>Name: {data.name}</h3>
+            <h3>Age: {data.age}</h3>
+            <h3>Email: {data.email}</h3>
+        </div>
+    )
+}
+export default User;
---
+++ pasteInApp.jsx/CompReuse.jsx
@@ -0,0 +1,34 @@
+import User from "../User";
+
+function CompReuse(){
+    const userData = [
+        {
+            name: "Aman",
+            email: "aman@gmail.com",
+            id: 1
+        },
+        {
+            name: "Arjun",
+            email: "arjun@gmail.com",
+            id: 2
+        },
+        {
+            name: "Abhimanyu",
+            email: "abhimanyu@gmail.com",
+            id: 3
+        }
+    ]
+    return( 
+        <div>
+            <h3>Reuse Component in Logo</h3>
+            {
+                userData.map((user)=>(
+                    <div key={user.id}>
+                        <User data={user}/>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+export default CompReuse;
