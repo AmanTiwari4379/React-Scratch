80dee3a5326710a419129545fdce87a743c0f1c0
AmanTiwari4379 <taman4379@gmail.com>
2025-09-23 16:17:12 +0530

    Added Nested Loop Map project

---
+++ pasteInApp.jsx/NestLoopMap NestLoopMap.jsx
@@ -0,0 +1,84 @@
+import College from "../NestLoopMap/College";
+
+
+function NestLoopMap() {
+  const collegeData = [
+    {
+      name: "IIT Alwar",
+      city: "Alwar",
+      website: "www.iita.com",
+      student: [
+        {
+          name: "Aman",
+          age: 23,
+          email: "xyzgmail.com",
+        },
+        {
+          name: "Abhimanyu",
+          age: 19,
+          email: "xyzgmail.com",
+        },
+        {
+          name: "Arjun",
+          age: 35,
+          email: "xyzgmail.com",
+        },
+      ],
+    },
+    {
+      name: "IIT Delhi",
+      city: "Delhi",
+      website: "www.iitd.com",
+      student: [
+        {
+          name: "Aman",
+          age: 23,
+          email: "xyzgmail.com",
+        },
+        {
+          name: "Abhimanyu",
+          age: 19,
+          email: "xyzgmail.com",
+        },
+        {
+          name: "Arjun",
+          age: 35,
+          email: "xyzgmail.com",
+        },
+      ],
+    },
+    {
+      name: "IIT Mumbai",
+      city: "Mumbai",
+      website: "www.iitm.com",
+      student: [
+        {
+          name: "Aman",
+          age: 23,
+          email: "xyzgmail.com",
+        },
+        {
+          name: "Abhimanyu",
+          age: 19,
+          email: "xyzgmail.com",
+        },
+        {
+          name: "Arjun",
+          age: 35,
+          email: "xyzgmail.com",
+        },
+      ],
+    },
+  ];
+  return (
+    <div>
+      <h3>Nested Looping with Component</h3>
+      {collegeData.map((college, index) => (
+        <div key={index}>
+          <College college={college}/>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default NestLoopMap;
