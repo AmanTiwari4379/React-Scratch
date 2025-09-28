a765cc0d10b0a90a6914937d71cfe92227efc3ce
AmanTiwari4379 <taman4379@gmail.com>
2025-09-28 15:51:58 +0530

    useRef demo added

---
+++ pasteInApp.jsx/UseRefDemo.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function UseRefDemo() {
+    const inputRef = useRef(null);
+    const inputHandler = ()=>{
+        console.log(inputRef);
+        inputRef.current.focus();
+        inputRef.current.style.color='red'
+    }
+    const toggleHandler = ()=>{
+        if(inputRef.current.style.display!='none'){
+            inputRef.current.style.display='none'
+        }else{
+            inputRef.current.style.display='inline'
+        }
+    }
+  return (
+    <>
+      <h3>useRef</h3>
+      <input type="text" placeholder='Enter your name'/>
+      <button onClick={inputHandler}>Focus on Input field</button>
+    </>
+  )
+}
+
+export default UseRefDemo
